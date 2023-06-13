2e6f55cba0f861075846016ecc041d51cb556184
Your Name <shubhamverma6351@gmail.com>
2023-06-13 21:56:57 +0530

    added backend link

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = "https://sharelink-server-1.onrender.com/";
+const API_URI = "https://sharelink-server-1.onrender.com";
 
 export const uploadFile = async (data) => {
     try {
